7dee1bf635f09b238a8e430ec94f6c635eaba879
Gabriel Silva <sr.silvagabriel@gmail.com>
2022-02-14 21:25:26 -0300

    ajuste de rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function Menu() {
     return(
         <div className="menu">
-            <Link className="social" to="/login">
+            <Link className="social" to="/">
                 <button><FiPower color="#fff" size={24} /></button>
             </Link>
             <a className="social" href="https://www.instagram.com/ssr.silvaaa/">
